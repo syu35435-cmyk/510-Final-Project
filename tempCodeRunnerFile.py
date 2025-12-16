629750d14807b64471103b95d1ad84606c744e88
syu35435-cmyk <syu35435@usc.edu>
2025-12-15 23:57:30 -0800

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-    for category in df_filtered["category"].unique():
-        print("\nCategory:", category)
-        print(
-            df_filtered[df_filtered["category"] == category][cols].mean()
-        )
